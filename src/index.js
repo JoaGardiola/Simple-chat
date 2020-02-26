86518fef927be40ea2cdfd1545c69fa436c6be41
JoaGardiola <joa.gardiola@gmail.com>
2020-02-26 10:48:20 -0300

    applying standard

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import { ApolloServer } from 'apollo-server-express'
 import { createServer } from 'http'
 
 import passport from 'passport'
-import {Strategy as JwtStrategy} from 'passport-jwt'
-import {ExtractJwt} from 'passport-jwt'
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 
 import schema from './schema'
 
@@ -14,21 +13,21 @@ const port = process.env.PORT || 3001
 
 const opts = {}
 
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = 'secret'
 
-passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-  User.findOne({id: jwt_payload.sub}, function(err, user) {
+passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+  User.findOne({ id: jwt_payload.sub }, function (err, user) {
     if (err) {
-      return done(err, false);
-      }
-      if (user) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-  });
-}));
+      return done(err, false)
+    }
+    if (user) {
+      return done(null, user)
+    } else {
+      return done(null, false)
+    }
+  })
+}))
 
 passport.use(JwtStrategy)
 
